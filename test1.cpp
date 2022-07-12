d03178d1b795805bd2927f1bc54017db2f602bcd
rakesh <rakeshdebnath12910@gmail.com>
2022-07-12 14:09:16 +0600

    branching from terminal

--- test1.cpp
+++ test1.cpp
@@ -5,5 +5,6 @@ int main()
     cout<<"Hello world"<<endl;
     cout<<"pushing to remote repository"<<endl;
     cout<<"creating a branch"<<endl;
+    cout<<"branching from terminal"<<endl;
     return 0;
 }
